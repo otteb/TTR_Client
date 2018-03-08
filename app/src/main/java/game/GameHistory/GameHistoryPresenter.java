6e5f52fcfcfeb555fb2945d4f71f7d6957fc78fb
otteb <brian.r.otte20@gmail.com>
2018-03-08 11:21:03 -0700

    updated GameHistoryPresenter

--- app/src/main/java/game/GameHistory/GameHistoryPresenter.java
+++ app/src/main/java/game/GameHistory/GameHistoryPresenter.java
@@ -1,6 +1,8 @@
 package game.GameHistory;
 
+import android.app.Activity;
 import android.content.Context;
+import android.widget.Toast;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -8,6 +10,7 @@ import java.util.Observer;
 import Interfaces.IGameHistoryPresenter;
 import Models.Gameplay.GameHistory;
 import Services.GUI.GameGuiFacade;
+import activities.MainActivity;
 import game.Chat.ChatFragment;
 
 /**
@@ -18,6 +21,8 @@ public class GameHistoryPresenter implements IGameHistoryPresenter, Observer {
     public Context context;
     public GameGuiFacade gameGuiFacade = new GameGuiFacade();
     GameHistoryPresenter gameHistoryPresenter = new GameHistoryPresenter(context);
+    //create instance of the GameHistory Fragment:
+    MainActivity mainActivity;
     //Constructor:
     public GameHistoryPresenter(Context c){
         this.context = c;
@@ -25,23 +30,36 @@ public class GameHistoryPresenter implements IGameHistoryPresenter, Observer {
     }
 
     //TODO
-    public void displayGameHistory(){
-
+    public void switchToGame(Context c){
+        context=c;
+        Toast.makeText(c, "Switching to Game", Toast.LENGTH_SHORT).show();
+        mainActivity = (MainActivity) context;
+        mainActivity.switchToRegister();
     }
 
     //TODO
-    public void switchToGame(){
-
+    public void switchToChat(Context c){
+        context=c;
+        Toast.makeText(c, "Switching to Chat", Toast.LENGTH_SHORT).show();
+        mainActivity = (MainActivity) context;
+        mainActivity.switchToRegister();
     }
 
-    //TODO
-    public void switchToStats(){
 
-    }
-
-    //TODO - honestly have no idea:
+    //TODO - honestly do not know hahaheheheAAAHHHHHHH!!!!!
     @Override
-    public void update(Observable observable, Object o) {
-
+    public void update(Observable observable, Object result) {
+        //TODO - Make sure the observable
+        if(result.equals("updateHistory"))
+        {
+            mainActivity = (MainActivity)((Activity)context);
+            mainActivity.update();
+//            lobbyFragment.updatePlayers();
+        }
+        else
+        {
+            Toast.makeText(context, (CharSequence) result, Toast.LENGTH_SHORT).show();
+        }
+        observable.hasChanged();
     }
 }
