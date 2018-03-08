2b5591c44613cb462724f17d2f508f6da526c899
otteb <brian.r.otte20@gmail.com>
2018-03-07 17:22:42 -0700

    presenter stubbs added

--- app/src/main/java/game/GamePresenter.java
+++ app/src/main/java/game/GamePresenter.java
@@ -1,8 +1,12 @@
 package game;
 
 import android.content.Context;
+import android.widget.Toast;
+
+import java.util.Observable;
 
 import Models.Result;
+import activities.MainActivity;
 
 /**
  * Created by fjameson on 2/28/18.
@@ -28,5 +32,26 @@ public class GamePresenter {
     {
         return null;
     }
+
+
+
+//    //TODO change the update for the GamePresenter
+//    @Override
+//    public void update(Observable o, Object authToken) {
+//
+//        if (!authToken.equals("create") && !authToken.equals("join") && !authToken.equals("start")) {
+//            if (user != null) {
+//                if (authToken.equals("ERROR: Invalid Registration") || authToken.equals("ERROR: Incorrect username/password combination")) {
+//                    Toast.makeText(context, (CharSequence) authToken, Toast.LENGTH_SHORT).show();
+//                }
+//                else {
+//                    user.setAuthToken((String) authToken);
+//                    mainActivity = (MainActivity) context;
+//                    mainActivity.switchToLobby(user);
+//                    user = null;
+//                }
+//            }
+//        }
+//    }
 
 }
