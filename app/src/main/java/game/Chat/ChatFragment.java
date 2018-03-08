2c368bdac3d14fc16ffe198adc831311eae4dbbd
otteb <brian.r.otte20@gmail.com>
2018-03-07 18:36:14 -0700

    added Presenter and Fragment Structure

---
+++ app/src/main/java/game/Chat/ChatFragment.java
@@ -0,0 +1,10 @@
+package game.Chat;
+
+import android.app.Fragment;
+
+/**
+ * Created by brianotte on 3/7/18.
+ */
+
+public class ChatFragment extends Fragment {
+}
---
+++ app/src/main/java/game/GameHistory/GameHistoryPresenter.java
@@ -0,0 +1,8 @@
+package game.GameHistory;
+
+/**
+ * Created by brianotte on 3/7/18.
+ */
+
+public class GameHistoryPresenter {
+}
