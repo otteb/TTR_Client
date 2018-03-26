d49b334c9f861f62689a5d99104ee678cc3e1e79
Finn <jayspark57@gmail.com>
2018-03-26 17:22:25 -0600

    added a better map

--- app/src/main/java/game/GameFragment.java
+++ app/src/main/java/game/GameFragment.java
@@ -192,8 +192,11 @@ public class GameFragment extends Fragment implements  View.OnTouchListener {
     public boolean onTouch(View v, MotionEvent event) {
         if(event.getAction()== MotionEvent.ACTION_DOWN) {
             routeNumber = gamePresenter.selectingRoute(event.getX(), event.getY());
+            String s = String.valueOf(event.getX())+" "+String.valueOf(event.getY());
+            Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
             if (routeNumber != null)
             {
+                Toast.makeText(getContext(), routeNumber.getName(), Toast.LENGTH_SHORT).show();
                 claimRoute.setVisibility(v.VISIBLE);
             }
             else {
